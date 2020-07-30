4d1e1e1b35d4eda466c5862471cd5e5cd1c852dd
saravanan <saravanan.r@technoduce.com>
2020-07-30 15:59:31 +0530

    clone

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -3,9 +3,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
+            'dsn' => 'mysql:host=148.72.88.28;dbname=trade',
+            'username' => 'trade_dev',
+            'password' => 'z?4A0T&9c]cB',
             'charset' => 'utf8',
         ],
         'mailer' => [
